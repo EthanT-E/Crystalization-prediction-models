6e6e15b577caaa5a1318db6c45738772089d80e3
EthanT-E <trippedwardse@gmail.com>
2025-11-10 14:51:02 +0000

    added supplier to data reading

--- first.py
+++ first.py
@@ -1,23 +1,17 @@
 from rdkit import Chem
-from rdkit.Chem import Draw
 from rdkit.Chem import Descriptors
 import pandas as pd
 
 df = pd.read_csv("data/Dataset_dHm.csv")
-smiles_list = df["SMILES"].tolist()
-mol_list = []
-desc_list = []
+df = pd.DataFrame(df["SMILES"])
+df["names"] = "unkown"
+df.to_csv("data/Smiles_dHm.csv", index=False)
 
-for smiles_str in smiles_list:
-    mol = Chem.MolFromSmiles(smiles_str)
-    desc_list.append(Descriptors.CalcMolDescriptors(mol))
+supplier = Chem.SmilesMolSupplier(
+    'data/Smiles_dHm.csv', delimiter=",", smilesColumn=0)
+desc = []
+for mol in supplier:
+    desc.append(Descriptors.CalcMolDescriptors(mol))
 
-df = pd.DataFrame(desc_list)
-df.to_csv("data/rdkit_unscaled.csv")
-# for smiles_str in range(0, 10):
-#     print(smiles_list[smiles_str])
-#     mol = Chem.MolFromSmiles(smiles_list[smiles_str])
-#     desc_list.append(Descriptors.CalcMolDescriptors(mol))
-# for i in range(0, len(smiles_list)):
-#     mol_list.append(Chem.MolFromSmiles(smiles_list[i]))
-#
+desc_df = pd.DataFrame(desc)
+desc_df.to_csv("data/test.csv")
