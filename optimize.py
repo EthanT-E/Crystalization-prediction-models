23c4684c4c1a669ac736c007f8e605437a7da31c
EthanT-E <trippedwardse@gmail.com>
2025-11-11 09:51:07 +0000

    Added opt_history plots

--- optimize.py
+++ optimize.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import root_mean_squared_error
+from sklearn.metrics import root_mean_squared_error, mean_absolute_error
 import xgboost as xgb
-import matplotlib.pyplot as plt
 import optuna as op
+import matplotlib.pyplot as plt
 
 
 def objective(trial):
@@ -24,17 +24,31 @@ def objective(trial):
     model = xgb.XGBRegressor(**param)
     model.fit(x_train, y_train)
     pred = model.predict(x_test)
-    return root_mean_squared_error(y_test, pred)
+    return root_mean_squared_error(y_test, pred), mean_absolute_error(y_test, pred)
 
 
 if __name__ == '__main__':
-    study = op.create_study(direction="minimize")
-    study.optimize(objective, n_trials=100, timeout=600)
-    print("Number of finished trials: ", len(study.trials))
-    print("Best trial:")
-    trial = study.best_trial
+    study = op.create_study(directions=["minimize", "minimize"])
+    op.logging.set_verbosity(op.logging.WARNING)
+    study.optimize(objective, n_trials=500,
+                   timeout=600, show_progress_bar=True)
+    trials = study.best_trials
+    best_trial_index = 0
+    min_rmse = 10000
+    for index in range(0, len(trials)):
+        rmse = trials[index].values[0]
+        if min_rmse > rmse:
+            best_trial_index = index
 
-    print("  Value: {}".format(trial.value))
+    best_trial = trials[best_trial_index]
+    print("Best trial:")
+    print(f"  Value: {best_trial.values}")
     print("  Params: ")
-    for key, value in trial.params.items():
-        print("    {}: {}".format(key, value))
+    for key, value in best_trial.params.items():
+        print(f"    {key}: {value}")
+
+    fig = op.visualization.matplotlib.plot_optimization_history(
+        study, target=lambda t: t.values[0], target_name="SMRE")
+    fig = op.visualization.matplotlib.plot_optimization_history(
+        study, target=lambda t: t.values[1], target_name="MAE")
+    plt.show()
