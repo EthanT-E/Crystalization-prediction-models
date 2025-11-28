09c3f62ff7e15ecdcd4ab66525d3acc057aff637
EthanT-E <trippedwardse@gmail.com>
2025-11-28 09:30:06 +0000

    made added qol and user freindliness

--- Model_maker.py
+++ Model_maker.py
@@ -1,40 +1,74 @@
 import pandas as pd
+import numpy as np
+from rdkit.ML.Descriptors.MoleculeDescriptors import MolecularDescriptorCalculator
+from rdkit import Chem
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import root_mean_squared_error, mean_absolute_error
 import xgboost as xgb
-from pickle import dump
+import sys
 
 
-def main():
+def Get_descriptor_df(Path_to_Smiles_list, Path_to_desc_list):
+
+    good_desc = pd.read_csv('descriptor/dhm_tsne.csv')
+    good_desc = good_desc.columns.to_list()
+    calculator = MolecularDescriptorCalculator(good_desc)
+    supplier = Chem.SmilesMolSupplier(
+        Path_to_Smiles_list, delimiter=",", smilesColumn=0)
+
+    # Getting molecules from input file
+    calced_desc_dic = []
+    for mol in supplier:
+        calced_desc_dic.append(calculator.CalcDescriptors(mol))
+    # Calculate descriptors of inputs
+    calced_desc_df = pd.DataFrame(calced_desc_dic)
+    print("Descriptors calculated")
+    return calced_desc_df
+
+
+def main(Path_to_Smiles_list='data/Smiles_dHm.csv', Path_to_desc_list='descriptor/dhm_tsne.csv', plotflag=0):
     target_df = pd.read_csv('data/Dataset_dHm.csv')
-    desc_df = pd.read_csv('data/filt_desc.csv')
+    # desc_df = pd.read_csv('data/tsne_dhm_desc.
+    # desc_df = Get_descriptor_df(
+    #    'data/Smiles_dHm.csv', 'descriptor/dhm_tsne.csv')
+    desc_df = Get_descriptor_df(
+        Path_to_Smiles_list, Path_to_desc_list)
     x_train, x_test, y_train, y_test = train_test_split(
         desc_df, target_df["dHm"], test_size=0.3, train_size=0.7)
 
-    model = xgb.XGBRegressor(max_depth=8,
-                             learning_rate=0.02,
-                             n_estimators=136,
-                             subsample=0.77,
-                             colsample_bytree=0.84,
-                             min_child_weight=4,
-                             gamma=4.409,
-                             alpha=0.54
+    model = xgb.XGBRegressor(max_depth=9,
+                             learning_rate=0.03751292045151882,
+                             n_estimators=392,
+                             subsample=0.6288953935334737,
+                             colsample_bytree=0.6103729030584606,
+                             min_child_weight=8,
+                             gamma=4.531330426603764,
+                             alpha=0.9572092266603516,
+                             tree_method='hist'
                              )
     model.fit(x_train, y_train)
     pred = model.predict(x_test)
     print(f"SMRE: {root_mean_squared_error(y_test, pred)}, MAE {
           mean_absolute_error(y_test, pred)}")
-    download = input("Download Model? [yes/no/exit]")
+    if plotflag == 1:
+        residuals = y_test - pred
+        plt.scatter(x=np.arange(0, len(y_test)), y=residuals)
+        plt.show()
+    download = input("Download Model? [yes/exit] ")
     if download == "yes":
-        model.save_model('models/dHm_model.json')
-        return False
-    elif download == 'exit':
-        return False
-    else:
-        return True
+        model.save_model('models/dHm_model_tsne.json')
 
 
 if __name__ == '__main__':
-    repeat = True
-    while (repeat == True):
-        repeat = main()
+    if len(sys.argv) == 1:
+        main()
+    elif len(sys.argv) == 2:
+        if (int(sys.argv[1]) != 1):
+            print(f"Usage: {
+                  sys.argv[0]}, <PLOTFLAG>\nPlot flag is by default 0 if you want to print residuals set to 1")
+        else:
+            main(1)
+    else:
+        print(f"Usage: {
+            sys.argv[0]}, <PLOTFLAG>\nPlot flag is by default 0 if you want to print residuals set to 1")
--- use_model.py
+++ use_model.py
@@ -2,38 +2,65 @@ from rdkit.ML.Descriptors.MoleculeDescriptors import MolecularDescriptorCalculat
 from rdkit import Chem
 import xgboost as xgb
 import pandas as pd
+import re
+import sys
 
-# Get the used descriptors
-good_desc = pd.read_csv('descriptor/dhm_descriptors.csv')
-good_desc = good_desc.columns.to_list()
 
-# RDKit descriptor calculator made
-calculator = MolecularDescriptorCalculator(
-    good_desc)  # only calcs the given descs
-# Get the input set
-supplier = Chem.SmilesMolSupplier(
-    'data/input_set.csv', delimiter=",", smilesColumn=0)
+def main(Path_to_inputs, Output_File_Name='dHm_predicted.csv'):
+    # Get the used descriptors
+    good_desc = pd.read_csv('descriptor/dhm_tsne.csv')
+    good_desc = good_desc.columns.to_list()
 
-# Getting molecules from input file
-input_dic = []
-for mol in supplier:
-    input_dic.append(calculator.CalcDescriptors(mol))
+    # RDKit descriptor calculator made
+    calculator = MolecularDescriptorCalculator(
+        good_desc)  # only calcs the given descs
+    # Get the input set
+    # supplier = Chem.SmilesMolSupplier(
+    #     'data/input_set.csv', delimiter=",", smilesColumn=0)
+    supplier = Chem.SmilesMolSupplier(
+        Path_to_inputs, delimiter=",", smilesColumn=0)
 
-# Calculate descriptors of inputs
-calced_desc_df = pd.DataFrame(input_dic)
+    # Getting molecules from input file
+    input_dic = []
+    for mol in supplier:
+        input_dic.append(calculator.CalcDescriptors(mol))
 
-# load model
-model = xgb.XGBRegressor()
-model.load_model('models/dHm_model.json')
+    # Calculate descriptors of inputs
+    calced_desc_df = pd.DataFrame(input_dic)
+    # calced_desc_df.to_csv("data/input_set_desc_unscaled.csv")
 
-# Set model column names
-calced_desc_df.columns = model.get_booster().feature_names
-predicted = model.predict(calced_desc_df)
+    # load model
+    model = xgb.XGBRegressor()
+    model.load_model('models/dHm_model_tsne.json')
+    print('model loaded')
 
-output_df = pd.read_csv("data/input_set.csv")
-output_df = output_df["NAME"]
+    # Set model column names
+    calced_desc_df.columns = model.get_booster().feature_names
+    predicted = model.predict(calced_desc_df)
+    print('prediction made')
 
-predict_df = pd.DataFrame([output_df, predicted])
-predict_df = predict_df.T
-predict_df.columns = ["Name", "Predicted dHm / kJ mol-1"]
-predict_df.to_csv('output/dHm.csv', index=False)
+    # output_df = pd.read_csv("data/input_set.csv")
+    output_df = pd.read_csv(Path_to_inputs)
+    output_df = output_df["NAME"]
+
+    predict_df = pd.DataFrame([output_df, predicted])
+    predict_df = predict_df.T
+    predict_df.columns = ["Name", "Predicted dHm / kJ mol-1"]
+    output_path = 'output/'+Output_File_Name
+    predict_df.to_csv(output_path, index=False)
+    print('Output written\nFin')
+
+
+if __name__ == '__main__':
+    print(f"Usage: {
+          sys.argv[0]} <PATH_TO_INPUT_FILE> <NAME_OF_OUTPUT_FILE>\nBy Defualt output file is called dHm results")
+    if len(sys.argv) == 2:
+        main(sys.argv[1])
+    elif len(sys.argv) == 3:
+        if (sys.argv[2].find('.csv') == -1 or sys.argv[2] == -1):
+            output_file_name = sys.argv[2] + '.csv'
+        else:
+            output_file_name = sys.argv[2]
+        main(sys.argv[1], output_file_name)
+    else:
+        print("Please input input file path")
